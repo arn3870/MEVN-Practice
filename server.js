ba47f9889585a71837359d27e9ae02423ce8749a
Arslan Baig <arn3870@gmail.com>
2022-12-26 16:15:27 +0500

    routing added

--- server.js
+++ server.js
@@ -11,17 +11,52 @@ const server = http.createServer((req, res) => {
 //   res.write('<h1>hello, friends</h1>');
 //   res.write('<p>hello again, friends</p>');
 
-fs.readFile('./views/index.html', (err, data) => {
-    if (err) {
-console.log(err);
-    }
-    else{
+// fs.readFile('./views/index.html', (err, data) => {
+//     if (err) {
+// console.log(err);
+//     }
+//     else{
 
-    }
-})
+//     }
+// })
 
 
-  res.end();
+//   res.end();
+// });
+
+//routing
+
+let path = './views/';
+switch(req.url) {
+  case '/':
+    path += 'index.html';
+    res.statusCode = 200;
+    break;
+  case '/about':
+    path += 'about.html';
+    res.statusCode = 200;
+    break;
+  case '/about-us':
+    res.statusCode = 301;
+    res.setHeader('Location', '/about');
+    res.end();
+    break;
+  default:
+    path += '404.html';
+    res.statusCode = 404;
+}
+
+// send html
+fs.readFile(path, (err, data) => {
+  if (err) {
+    console.log(err);
+    res.end();
+  }
+  //res.write(data);
+  res.end(data);
+});
+
+
 });
 
 server.listen(3000, "localhost", () => {
