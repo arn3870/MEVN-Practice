0a95c094a5b1b8ae4717cb9a0ad7d857574959b4
Arslan Baig <arn3870@gmail.com>
2023-01-03 22:03:42 +0500

    folder structuring

---
+++ streams-and-piping/streams.js
@@ -0,0 +1,18 @@
+//read and write data using streams
+
+const fs = require('fs');
+
+
+const readStream = fs.createReadStream('../File-system-module/docs/blog3.txt', { encoding: 'utf-8' });
+const writeStream = fs.createWriteStream('../File-system-Modulw/docs/blog4.txt')
+
+// readStream.on('data', (chunk) => {
+//   console.log('---- NEW CHUNK ----');
+//   console.log(chunk);
+//   writeStream.write('\nNEW CHUNK:\n');
+//   writeStream.write(chunk);
+// });
+
+// piping
+//writing data that it recieves from the read stream
+readStream.pipe(writeStream);
