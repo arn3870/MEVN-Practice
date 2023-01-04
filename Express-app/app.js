dbccda62a34bb2b45dee0db1287e666819c14355
Arslan Baig <arn3870@gmail.com>
2023-01-04 23:21:16 +0500

    View Engine

--- Express-app/app.js
+++ Express-app/app.js
@@ -1,25 +1,30 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.listen(3000);
 
 //routing in express
 app.get("/", (req, res) => {
-  res.sendFile("D:/cloudtek/MEVN/node-server-setup/views/index.html");
+  res.render("index");
 });
 app.get("/about", (req, res) => {
-  res.sendFile("D:/cloudtek/MEVN/node-server-setup/views/about.html");
+  res.render("about");
+});
+
+app.get("/404", (req, res) => {
+  res.render("404");
 });
 
 //redirecting to a route
 
-app.get("/home", (req, res) => {
-  res.redirect("/");
-})
+// app.get("/home", (req, res) => {
+//   res.redirect("/");
+// })
 
 //404 page
-//app.use gets fired on every request and if there's no route matched it shows up
+//app.use gets fired on every request. and if there's no route matched it shows up
 app.use((req, res) => {
   res.status(404).sendFile("D:/cloudtek/MEVN/node-server-setup/views/404.html");
 })
