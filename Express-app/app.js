5dcd9faed935782472d06a1be50610847ab8b9e3
Arslan Baig <arn3870@gmail.com>
2023-01-04 19:46:04 +0500

    express basic covered

--- Express-app/app.js
+++ Express-app/app.js
@@ -4,6 +4,22 @@ const app = express();
 
 app.listen(3000);
 
+//routing in express
 app.get("/", (req, res) => {
-  res.send("<h1>Homepage</h1>");
+  res.sendFile("D:/cloudtek/MEVN/node-server-setup/views/index.html");
 });
+app.get("/about", (req, res) => {
+  res.sendFile("D:/cloudtek/MEVN/node-server-setup/views/about.html");
+});
+
+//redirecting to a route
+
+app.get("/home", (req, res) => {
+  res.redirect("/");
+})
+
+//404 page
+//app.use gets fired on every request and if there's no route matched it shows up
+app.use((req, res) => {
+  res.status(404).sendFile("D:/cloudtek/MEVN/node-server-setup/views/404.html");
+})
--- node-server-setup/server.js
+++ node-server-setup/server.js
@@ -1,64 +1,64 @@
-// //making server
-// const http = require("http");
-// //we need a file system to return html files
-// const fs = require('fs')
-// const _ = require('lodash') //just for practice purpose. you can install any package of your choice
+//making server
+const http = require("http");
+//we need a file system to return html files
+const fs = require('fs')
+const _ = require('lodash') //just for practice purpose. you can install any package of your choice
 
-// const server = http.createServer((req, res) => {
-//   console.log(req.url, req.method);
-//   //set header content type
-//   res.setHeader("Content-Type", "text/html");
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  //set header content type
+  res.setHeader("Content-Type", "text/html");
 
-// //   res.write('<h1>hello, friends</h1>');
-// //   res.write('<p>hello again, friends</p>');
+//   res.write('<h1>hello, friends</h1>');
+//   res.write('<p>hello again, friends</p>');
 
-// // fs.readFile('./views/index.html', (err, data) => {
-// //     if (err) {
-// // console.log(err);
-// //     }
-// //     else{
+// fs.readFile('./views/index.html', (err, data) => {
+//     if (err) {
+// console.log(err);
+//     }
+//     else{
 
-// //     }
-// // })
+//     }
+// })
 
 
-// //   res.end();
-// // });
-
-// //routing
-
-// let path = './views/';
-// switch(req.url) {
-//   case '/':
-//     path += 'index.html';
-//     res.statusCode = 200;
-//     break;
-//   case '/about':
-//     path += 'about.html';
-//     res.statusCode = 200;
-//     break;
-//   case '/about-us':
-//     res.statusCode = 301;
-//     res.setHeader('Location', '/about');
-//     res.end();
-//     break;
-//   default:
-//     path += '404.html';
-//     res.statusCode = 404;
-// }
-// // send html
-// fs.readFile(path, (err, data) => {
-//   if (err) {
-//     console.log(err);
-//     res.end();
-//   }
-//   //res.write(data);
-//   res.end(data);
+//   res.end();
 // });
 
+//routing
 
-// });
+let path = './views/';
+switch(req.url) {
+  case '/':
+    path += 'index.html';
+    res.statusCode = 200;
+    break;
+  case '/about':
+    path += 'about.html';
+    res.statusCode = 200;
+    break;
+  case '/about-us':
+    res.statusCode = 301;
+    res.setHeader('Location', '/about');
+    res.end();
+    break;
+  default:
+    path += '404.html';
+    res.statusCode = 404;
+}
+// send html
+fs.readFile(path, (err, data) => {
+  if (err) {
+    console.log(err);
+    res.end();
+  }
+  //res.write(data);
+  res.end(data);
+});
 
-// server.listen(3000, "localhost", () => {
-//   console.log("listening for requests on port 3000");
-// });
+
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
+});
